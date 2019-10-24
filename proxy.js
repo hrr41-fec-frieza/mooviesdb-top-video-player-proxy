f62e63b76e53d554379b5f19a5d4bd1489ccb1ec
Michael Scanza <mscanza@gmail.com>
2019-10-23 21:59:32 -0700

    Update proxy server

---
+++ proxy.js
@@ -0,0 +1,9 @@
+var proxy = require('express-http-proxy');
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/client/src'))
+
+
+app.listen(8000, () => {console.log('Proxy running on port 8000!')})
