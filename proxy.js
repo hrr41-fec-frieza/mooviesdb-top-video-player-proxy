8a2ccb34763472d504eb52b6868627421a84154a
Michael Scanza <mscanza@gmail.com>
2019-10-24 19:33:14 -0700

    Update proxy to include id endpoint

--- proxy.js
+++ proxy.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '/client/src'))
+app.use(express.static(__dirname + '/client/src'));
+
+app.use('/:id', express.static(__dirname + '/client/src'));
 
 
 app.listen(8000, () => {console.log('Proxy running on port 8000!')})
